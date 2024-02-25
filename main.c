d97900fab7c272174ed8fdcdc226cccc6e907458
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-02-25 15:08:50 +0300

    Remove unnecessary header files and update main.h
    This commit removes unnecessary header files from main.c and updates main.h to include the required header files.

--- main.c
+++ main.c
@@ -5,13 +5,6 @@
  * @av: the List of args
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
 #define MAX_INPUT_SIZE 1024
 
 int main() {
--- main.h
+++ main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #define MAX_SIZE 1024
 #endif
