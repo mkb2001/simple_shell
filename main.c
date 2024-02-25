d76cc216482411acea871356690933e628bf6e88
Kevin Mugarura B <kevinmugarura051@gmail.com>
2024-02-25 13:41:25 +0300

    Add input validation in main function

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
+#include "main.h"
 /**
  * main - This is the main function
  * @ac: The number of args 
  * @av: the List of args
 */
 
-int main(int ac, char **av)
+int main(int ac, char av[])
 {
+    char input[MAX_SIZE];
+    while (1)
+    {
+        printf("#cisfun$ ");
+        scanf("%s\n", input);
 
+        if (strcmp(input, " ") != 0)
+        {
+            printf("%s: No such directory", av[0] );
+        }
+     
+    }
 }
--- main.h
+++ main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX_SIZE 1024
 #endif
