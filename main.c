5fd53847fa78e19f8e2f65ce658b1d40ec201d8e
mkb2001 <kevinmugarura051@gmail.com>
2024-02-25 15:03:12 +0300

    Refactor main.c to implement a simple shell

--- main.c
+++ main.c
@@ -5,18 +5,52 @@
  * @av: the List of args
 */
 
-int main(int ac, char av[])
-{
-    char input[MAX_SIZE];
-    while (1)
-    {
-        printf("#cisfun$ ");
-        scanf("%s\n", input);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-        if (strcmp(input, " ") != 0)
-        {
-            printf("%s: No such directory", av[0] );
+#define MAX_INPUT_SIZE 1024
+
+int main() {
+    char input[MAX_INPUT_SIZE];
+
+    while (1) {
+        // Display prompt and wait for user input
+        printf("$ ");
+        if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
+            // Handle end of file condition (Ctrl+D)
+            printf("\nExiting simple_shell.\n");
+            break;
         }
-     
+
+        // Remove newline character from input
+        input[strcspn(input, "\n")] = '\0';
+
+        // Fork a child process
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // Child process: execute the command
+            if (execlp(input, input, (char *)NULL) == -1) {
+                // Print error if command is not found
+                perror("simple_shell");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            // Parent process: wait for the child to finish
+            int status;
+            if (waitpid(pid, &status, 0) == -1) {
+                perror("waitpid");
+                exit(EXIT_FAILURE);
+            }
+        }
     }
+
+    return 0;
 }
